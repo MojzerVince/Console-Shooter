2465fbe6fff471ed2d4b3ff58b4a791ecdbe96c8
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-06-26 13:12:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Shooter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
