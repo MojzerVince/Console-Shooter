b8cdf29ab27aac8253c1f30f6c917cb877802694
Mojzi <98553108+MojzerVince@users.noreply.github.com>
2024-06-26 13:26:25 +0200

    Target generation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            Console.WindowWidth = 150;
+            Console.WindowHeight = 50;
+
+            while (true)
+            {
+                Random r = new Random();
+                int x = r.Next(0, 150);
+                int y = r.Next(0, 25);
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" ");
+                Console.ResetColor();
+
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
         }
     }
 }
